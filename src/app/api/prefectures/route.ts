b6b7c5e5aa7732b21953b70cd95892a121eb3780
satooru65536 <ryuto4619@gmail.com>
2024-05-21 15:12:07 +0900

    feat: 都道府県を返す中間API

---
+++ src/app/api/prefectures/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+import { fetchPrefectures } from './prefectureFetcher';
+
+export async function GET(_req: Request): Promise<NextResponse> {
+  const prefectures = await fetchPrefectures();
+
+  if (prefectures === undefined) {
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+
+  return NextResponse.json({ prefectures }, { status: 200 });
+}
