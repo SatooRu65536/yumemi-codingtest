8a3cdc866c0095721cee41137ef9dd91a344efbb
satooru65536 <ryuto4619@gmail.com>
2024-05-22 10:42:30 +0900

    test: 都道府県選択周りのテスト追加

---
+++ src/app/_components/PrefsSection/List/Checkbox/index.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'jotai';
+import { act, type ReactElement } from 'react';
+import PrefCheckbox from '.';
+import useSelectedPrefs from '@/hooks/useSelectedPrefs';
+import { type Prefecture } from '@/types/prefecture';
+
+const pref: Prefecture = {
+  prefCode: 1,
+  prefName: '北海道',
+};
+
+function TestPrefCheckbox(): ReactElement {
+  const [selectedPrefCodes, changeSelectedState] = useSelectedPrefs();
+
+  return (
+    <Provider>
+      <PrefCheckbox
+        changeSelectedState={changeSelectedState}
+        checked={selectedPrefCodes.includes(pref.prefCode)}
+        pref={pref}
+      />
+    </Provider>
+  );
+}
+
+describe('components Header', () => {
+  test('チェックボックスを選択できるか', () => {
+    render(<TestPrefCheckbox />);
+
+    const checkbox = screen.getByRole<HTMLInputElement>('checkbox');
+
+    expect(checkbox.checked).toBe(false);
+    act(() => {
+      checkbox.click();
+    });
+    expect(checkbox.checked).toBe(true);
+  });
+});
