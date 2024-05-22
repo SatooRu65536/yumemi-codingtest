cee8a9b26c76fa9b331e19d000755ea5230d345a
satooru65536 <ryuto4619@gmail.com>
2024-05-22 09:28:32 +0900

    chore: storybookを追加

---
+++ src/app/_components/PrefsSection/List/checkbox.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta } from '@storybook/react';
+import { type ReactElement } from 'react';
+import PrefCheckbox from './checkbox';
+import useSelectedPrefs from '@/hooks/useSelectedPrefs';
+import { type Prefecture } from '@/types/prefecture';
+
+const meta = {
+  title: 'PrefCheckbox',
+  component: PrefCheckbox,
+  tags: ['autodocs'],
+} satisfies Meta<typeof PrefCheckbox>;
+
+export default meta;
+
+const pref: Prefecture = {
+  prefCode: 1,
+  prefName: '北海道',
+};
+
+function Template(): ReactElement {
+  const [selectedPrefCodes, changeSelectedState] = useSelectedPrefs();
+
+  return (
+    <PrefCheckbox
+      changeSelectedState={changeSelectedState}
+      checked={selectedPrefCodes.includes(pref.prefCode)}
+      pref={pref}
+    />
+  );
+}
+
+export const Default = Template.bind({});
