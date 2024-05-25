ab8dd99fddf29db5581e419875535138166e849d
satooru65536 <ryuto4619@gmail.com>
2024-05-26 00:54:38 +0900

    change: タイトル

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.scss';
 export default function Header(): ReactElement {
   return (
     <header className={styles.header}>
-      <h1>都道府県別総人口推移知れる.app</h1>
+      <h1>都道府県別人口推移比較</h1>
     </header>
   );
 }
