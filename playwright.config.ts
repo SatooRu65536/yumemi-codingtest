1469659cbbd6c0ef73fb65285396789389113021
satooru65536 <ryuto4619@gmail.com>
2024-05-26 21:25:00 +0900

    fix: playright のenvファイルを指定

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: '.env.test.local' });
 
 const baseURL = 'http://localhost:3000';
 
